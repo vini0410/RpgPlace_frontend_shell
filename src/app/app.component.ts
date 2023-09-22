3a2e3e8360154e628ceab3bf86a970d644364203
vini <vini0420@gmail.com>
2023-09-21 23:54:13 -0300

    config mfe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'RpgPlaceFrontendCore';
+  title = 'RpgPlace_frontend_shell';
 }
